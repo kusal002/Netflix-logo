146b394581ecd5b4c6004f5f030aa604ee438fb5
kusal002 <systematicshift002@gmail.com>
2024-05-20 23:31:29 +0530

    new changes

--- app.js
+++ app.js
@@ -1,11 +1,20 @@
-let audio = document.getElementById("netflix-sound");
-window.addEventListener("load", function () {
-  audio
-    .play()
-    .then(() => {
-      console.log("audio is playing");
-    })
-    .catch((error) => {
-      console.log("Audio play failed: " + error);
-    });
+let netflixintroTag = document.getElementsByTagName("netflixintro")[0];
+let audioTag = document.getElementById("netflix-sound");
+let buttonsTag = document.getElementById("buttons");
+let allWordBtns = Array.from(document.getElementsByClassName("word-btn"));
+let audio = new Audio(audioTag.src);
+
+allWordBtns.forEach((eachBtn) => {
+  eachBtn.addEventListener("click", () => {
+    netflixintroTag.setAttribute("letter", eachBtn.value);
+
+    netflixintroTag.style.display = "block";
+    buttonsTag.style.display = "none";
+    audio.play();
+
+    setTimeout(() => {
+      netflixintroTag.style.display = "none";
+      buttonsTag.style.display = "block";
+    }, 5000);
+  });
 });
